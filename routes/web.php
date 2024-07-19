06134665f9aadf326165ab387fd30d26bcfecf0e
Jazmin-Arcelly-Reyes-Guzman1 <t512700220@unitru.edu.pe>
2024-07-18 23:52:47 -0500

    first commit

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Servicios2Controller;
+use App\Http\Controllers\ContactoController;
+
+
+Route::view('/', 'home')->name('home');
+Route::resource('servicios','App\Http\Controllers\Servicios2Controller')->names('servicios')->middleware('auth');
+//ruta del controlador servicios usado
+//Route::get('servicios', 'App\Http\Controllers\Servicios2Controller@index')->name('servicios');
+
+// Route::get('servicios', 'App\Http\Controllers\Servicios2Controller@index')->name('servicios');
+// Route::get('servicios/crear', 'App\Http\Controllers\Servicios2Controller@create')->name('servicios.create');
+// //ruta editar
+// Route::get('servicios/{id}/editar', 'App\Http\Controllers\Servicios2Controller@edit')->name('servicios.edit');
+// //ruta modificar con el metodo parcial partch
+// Route::patch('servicios/{id}', 'App\Http\Controllers\Servicios2Controller@update')->name('servicios.update');
+// //ruta  store con metodo pos
+// Route::post('servicios', 'App\Http\Controllers\Servicios2Controller@store')->name('servicios.store');
+// Route::get('servicios/{id}', 'App\Http\Controllers\Servicios2Controller@show')->name('servicios.show');
+// //ruta eliminar con metodo destroy
+// Route::delete('servicios/{servicio}', 'App\Http\Controllers\Servicios2Controller@destroy')->name('servicios.destroy');
+Route::view('contacto', 'contacto')->name('contacto');
+//contacto
+Route::post('contacto', 'App\Http\Controllers\ContactoController@store');
+
+
+Auth::routes(['register'=> false]);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
